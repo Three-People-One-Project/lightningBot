a8ea67cb4cec76ce57f2424c7e9eb54c77ae60fc
leahgrace555 <leah.grace555@gmail.com>
2020-11-09 17:35:50 -0500

    set up database connection

---
+++ data/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO challenges(challenge_id) values ('582c297e56373f0426000098');
+
+INSERT INTO challenges(challenge_id) values ('57e5279b7cf1aea5cf000359');
+
+
+INSERT INTO challenges(challenge_id) values ('57b06f90e298a7b53d000a86');
+
+
+INSERT INTO challenges(challenge_id) values ('526233aefd4764272800036f');
+
+
+INSERT INTO challenges(challenge_id) values ('5efae11e2d12df00331f91a6');
---
+++ server.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const {Client,MessageEmbed} = require('discord.js')
+require('dotenv').config();
+const pg = require('pg');
+const pgclient = new pg.Client(process.env.DATABASE_URL);
+pgclient.connect().then(()=> {
+  console.log('connected to pg')
+})
+pgclient.on('error', err => console.error(err));
+const superagent = require('superagent');
+const client = new Client();
+
+client.on('ready', ()=> {
+  console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on('guildMemberAdd', (member)=> {
+  const channel = member.guild.channels.cache.find( channel => {
+    channel.name === 'general'
+  })
+  if(!channel) {
+    return;
+  }
+  channel.send(`Welcome to the war zone, ${member}`);
+})
+
+let duel = true;
+let challengeUser = ''
+
+client.on('message', (message)=> {
+  if(message.content==='!cc') {
+    let sql = 'SELECT * FROM challenges;';
+    pgclient.query(sql)
+      .then(query => {
+        let min = 0;
+        let max = query.rows.length;
+        let num = Math.floor(Math.random()*(max-min))
+        let headers = {Authorization: process.env.CODEWARS,
+        ContentType: "application/json"}
+        superagent.get(`https://www.codewars.com/api/v1/code-challenges/${query.rows[num].challenge_id}`).set(headers)
+        .then( results => {
+          console.log(typeof results.body.name);
+          console.log(results.body.name);
+          console.log(results.body.id);
+          let update = 'UPDATE challenges set challenge_name=$1 WHERE challenge_id=$2;';
+          let values = [results.body.name,results.body.id];
+          pgclient.query(update,values).then(()=>{
+            console.log('success')
+          }).catch(err => console.log(err))
+          let description = results.body.description.substr(0,500)
+          const embed = new MessageEmbed()
+          .setTitle(`${results.body.name} (click to view full challenge)`)
+          .setURL(results.body.url)
+          .setColor('#0099ff')
+          .setDescription(description)
+          .setFooter('Full challenge available through Code Wars');
+          message.channel.send(embed);
+      }).catch(err => console.log(err))
+      }).catch(err => {
+        console.log(err)
+      })
+  }
+  let challenge = message.content.split(' ')
+  if(challenge[0]==='!duel') {
+    challengeUser = challenge[1]
+    message.channel.send(`${challenge[1]}, you've been challenged to a duel by ${message.author.username}! reply !y to accept`)
+  }
+
+  if(message.content === '!y') {
+
+  }
+})
+
+client.login(process.env.TOKEN);
