8ea8d70a923ad281c81b35a4a9a801dfce929f01
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-11-12 16:19:56 -0800

    adjusted the output for help command

--- commands/help.js
+++ commands/help.js
@@ -20,16 +20,16 @@ function help(message, MessageEmbed){
             DSA = trees, hashset, linkedlist, queues, stacks, graphs
             Example:
             \`\`\`
-            !cc
-            !cc javascript
-            !cc c# trees
-            !cc javascript novice linkedlist
-            \`\`\`
+!cc
+!cc javascript
+!cc c# trees
+!cc javascript novice linkedlist
+\`\`\`
             **Knowledge**
             !k word = gives you information on the topic entered
             Example:
             \`\`\`
-            !k binary tree
+!k binary tree
 In computer science, a binary tree is a tree data structure in which each node has at most two children, which are referred to as the left child and the right child. A recursive definition using just set theory notions is that a (non-empty) binary tree is a tuple (L, S, R), where L and R are binary trees or the empty set and S is a singleton set containing the root.
             \`\`\`
             **Technical Question duel**
@@ -39,19 +39,19 @@ In computer science, a binary tree is a tree data structure in which each node h
             once the match is over the winner or loser will be saved.
             Example:
             \`\`\`
-            TrialByFire
-            !duel @Leah
-            testBot
-            @Leah, you've been challenged to a duel by @TrialbyFire! reply !duel !y to accept or reply !duel !n to deny
-            Leah
-            !duel !y
-            testbot
-            @Leah has accepted the challenge from @TrialbyFire
-            or if they deny:
-            Leah
-            !duel !n
-            @Leah has denied the challenge from @TrialbyFire
-            \`\`\`
+TrialByFire
+!duel @Leah
+testBot
+@Leah, you've been challenged to a duel by @TrialbyFire! reply !duel !y to accept or reply !duel !n to deny
+Leah
+!duel !y
+testbot
+@Leah has accepted the challenge from @TrialbyFire
+or if they deny:
+Leah
+!duel !n
+@Leah has denied the challenge from @TrialbyFire
+\`\`\`
             !stats allows you to see your total wins and losses against an individual
             Example: !stats @opponent will return stats between you and opponent
           `);
